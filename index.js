cab026ef57730f841f3fcd0f30f3857b55a5aa8d
MohannadHosam <Mohannad.hosam17@gmail.com>
2024-02-26 14:28:02 +0200

    new Relic configuration

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
 
+try {
+  require('newrelic');
+} catch (error) {
+  console.error('Error initializing New Relic:', error.message);
+}
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
+  const msg = 'Hello Node!\n';
   res.end(msg);
 });
 
---
+++ newrelic.js
@@ -0,0 +1,8 @@
+exports.config = {
+    app_name: ['hellonodeapp'],
+    license_key: 'eu01xxe1503a219c8eae3915784ebca6FFFFNRAL',
+    logging: {
+      level: 'info', // Adjust the logging level as needed
+    },
+  };
+  
