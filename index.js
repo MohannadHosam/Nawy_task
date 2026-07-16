1650f6b05c95dee55f2a818e53e5bdff8f5fae5e
MohannadHosam <Mohannad.hosam17@gmail.com>
2026-07-16 19:12:51 +0300

    added new line

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
 
-try {
-  require('newrelic');
-} catch (error) {
-  console.error('Error initializing New Relic:', error.message);
-}
+// try {
+//   require('newrelic');
+// } catch (error) {
+//   console.error('Error initializing New Relic:', error.message);
+// }
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n';
-  res.end(msg);
+  const msg = 'Hello Node version 3!\n';
+  const msg2 = 'This is the second message!\n';
+  res.end(msg + msg2);
 });
 
 server.listen(port, () => {
